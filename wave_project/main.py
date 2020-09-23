8e5372f9778f2d24331535199ddbd2253d8b6bcd
Lasse Steinnes <lasse.steinnes93@gmail.com>
2020-09-23 18:22:02 +0200

    functional solver

--- wave_project/main.py
+++ wave_project/main.py
@@ -1,1 +1,24 @@
 #### Main programme for running wave project ####
+import numpy as np
+#from visualize import Visualize
+from wave2D import Wave_2D_solver
+
+def f(x,y,t):
+    return 0
+
+def g(x,y):
+    return x
+
+def I(x,y):
+    return y
+
+def V(x,y):
+    return x
+
+Nx = 40; Ny = 40; T = 10; dt = 0.1
+Lx = 10; Ly = 10; b = 1;
+
+
+solver = Wave_2D_solver(Lx,Ly,Nx,Ny,T,dt)       # Initialize and create instance
+solver.set_inital(I)                           # Set t = 0
+u, t = solver.solve(b,g,f,V)                    # Solve the equation
--- wave_project/wave2D.py
+++ wave_project/wave2D.py
@@ -7,52 +7,102 @@ class Wave_2D_solver():
     boundary conditions (BC's) for a square domain (0,Lx)x(0,Ly).
     The method used is a finite (centered difference scheme.
     """
-    def __init__(self, b, g, f, I, V, Lx, Ly, Nx, Ny, T, dt):
+    def __init__(self, Lx, Ly, Nx, Ny, T, dt):
         """
         Initalizes parameters
         - g(x,y)
         - f(x,y)
         """
+        self.Nx, self.Ny = Nx, Ny                                     # x,y physical points only
         self.x = np.linspace(0,Lx,Nx+1);                # +1 because then we can use index Nx for last element
         self.y = np.linspace(0,Ly,Ny+1);                # same here
-        self.dx = x[1] -x[0]; self.dy = y[1] - y[0];    # Spacing homogenous
-        self.Nt = int(round(T/float(dt)))               # Integer number of time iterations
-        self.t = np.linspace(0,Nt*dt,N+1)               # time mesh
+        self.dx = self.x[1] - self.x[0];
+        self.dy = self.y[1] - self.y[0];                          # Spacing homogenous
+        Nt = int(round(T/float(dt)))                    # Integer number of time iterations
+        self.t = np.linspace(0,Nt*dt,Nt+1)              # time mesh
+        self.dt = dt
 
-        # Initialize vectors (2 dimensional mesh [i,j])
-        u_nn = np.zeros((Nx+3,Ny+3))           # time n-1, t -dt
-        u_n = np.zeros((Nx+3,Ny+3))            # time n, t
-        u = np.zeros((Nx+3,Ny+3))           # time n+1, t + dt
+        # Initialize vectors (2 dimensional mesh [i,j]) using ghost cells i = -1,0 ... Nx+1 and so on
+        self.u_nn = np.zeros((Nx+3,Ny+3))           # time n-1, so that t -dt
+        self.u_n = np.zeros((Nx+3,Ny+3))            # time n, so that
+        self.u = np.zeros((Nx+3,Ny+3))           # time n+1, so that t + dt
 
         # Index sets, only iterates over INNER points
-        Ix = range(1,u.shape[0]-1); Iy = range(1,u.shape[1]-1);
-        It = range(0,t.shape[0])
-
+        u_nn, u_n, u = self.u,self.u_n, self.u
+        self.Ix = range(1,u.shape[0]-1); self.Iy = range(1,u.shape[1]-1);
+        self.It = range(0,self.t.shape[0])
+        #print(self.Ix)
+        #print(self.Ix[1:-1])
         # Note: x[i-Ix[0]] Is the right index. Same for y
 
-    def set_inital(self): # Set inital condition for t = 0
+    def set_inital(self,I): # Set inital condition for t = 0
+        Ix = self.Ix; x = self.y; Iy = self.Iy; y = self.y
         for i in Ix:
             for j in Iy:
-                u_n[i,j] = I(x[i-Ix[0]],y[j-Iy[0]])
+                ix = i-Ix[0]; jy = j-Iy[0]
+                self.u_n[i,j] = I(x[ix],y[jy])
 
-    def advance(self,step1 = False):
+    def var_coeff(self,g,x,y,i,j):
+            dx, dy = self.dx, self.dy; u_n = self.u_n; Ix, Iy = self.Ix, self.Iy
+            ix = i-Ix[0]; jy = j-Iy[0]
+
+            if ix < self.Nx:
+                gx_rhalf = 1/2*(g(x[ix],y[jy]) + g(x[ix+1],y[jy]));
+            else:
+                gx_rhalf = 1/2*(g(x[ix],y[jy]) + g(x[ix-1],y[jy]))
+
+            if jy < self.Ny:
+                gy_rhalf = 1/2*(g(x[ix],y[jy]) + g(x[ix],y[jy+1]))
+            else:
+                gy_rhalf = 1/2*(g(x[ix],y[jy]) + g(x[ix],y[jy-1]))
+
+            gx_lhalf = 1/2*(g(x[ix],y[jy]) + g(x[ix-1],y[jy]))
+            gy_lhalf = 1/2*(g(x[ix],y[jy]) + g(x[ix],y[jy-1]))
+
+            self.g_ux_x = 1/dx**2*(gx_rhalf*(u_n[i+1,j]-u_n[i,j]) \
+                            - gx_lhalf*(u_n[i,j]-u_n[i-1,j]))
+            self.gy_u_y = 1/dy**2*(gy_rhalf*(u_n[i,j+1]-u_n[i,j]) \
+                            - gy_lhalf*(u_n[i,j]-u_n[i,j-1]))
+
+    def advance(self,n,b,g,f,V):
         """
         Advances the solver
         """
-        if step1:                   # write first step here (t = 1)
+        Ix = self.Ix; x = self.y; Iy = self.Iy; y = self.y
+        t = self.t
+        # first step(t = 1), <-- u_t initial condition V
+        if n==1:
             for i in Ix:
                 for j in Iy:
-                    u[i,j] =
+                    ix = i-Ix[0]; jy = j-Iy[0]
+                    self.var_coeff(g,x,y,i,j)
+                    g_ux_x = self.g_ux_x; gy_u_y = self.gy_u_y
 
+                    self.u[i,j] = 1/(2*b*self.dt)*(4*self.u_n[i,j] - V(x[ix],y[jy])) \
+                            + (self.dt/b)*(g_ux_x + gy_u_y + f(x[ix],y[jy],t[n]))
+
+        # All other times steps
         else:
             for i in Ix:
                 for j in Iy:
-                    # write other steps here
-        return u
 
+                    ix = i-Ix[0]; jy = j-Iy[0]
+                    self.var_coeff(g,x,y,i,j)
+                    g_ux_x = self.g_ux_x; g_uy_y = self.gy_u_y
+
+                    self.u[i,j] = 1/(2 + b*self.dt)*(self.u_nn[i,j]*(b*self.dt -2) \
+                    + 4*self.u_n[i,j] + (2*self.dt**2)*(g_ux_x + g_uy_y + f(x[ix],y[jy],t[n])))
 
         # Boundary condition which enforces Neuman condition du/dn = 0
-        i = Ix[0]; u[i-1,:] = u[i+1,:]         # x = 0
-        i = Ix[-1]; u[i+1,:] = u[i-1,:]        # x = Lx
-        j = Iy[0]; u[:,j-1] = u[:,j+1]         # y = 0
-        j = Iy[-1]; u[:,j+1] = u[:,j-1]        # y = Ly
+        i = Ix[0]; self.u[i-1,:] = self.u[i+1,:]         # x = 0
+        i = Ix[-1]; self.u[i+1,:] = self.u[i-1,:]        # x = Lx
+        j = Iy[0]; self.u[:,j-1] = self.u[:,j+1]         # y = 0
+        j = Iy[-1]; self.u[:,j+1] = self.u[:,j-1]        # y = Ly
+
+    def solve(self,b,g,f,V):
+        # Make solver for all time steps
+        for n in self.It[1:-1]:
+            self.advance(n,b,g,f,V)
+            self.u_nn, self.u_n, self.u = self.u_n, self.u, self.u_nn  # Update time vectors
+
+        return self.u, self.t
