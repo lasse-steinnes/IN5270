ef3d716cc51a887fee1ea5e12a400ffc301c58b7
Lasse Steinnes <lasse.steinnes93@gmail.com>
2020-11-09 16:08:51 +0100

    Solved Finite Element with P2 and l2norm

--- FEM1D_project/FEM_P2.py
+++ FEM1D_project/FEM_P2.py
@@ -1,5 +1,7 @@
 import sympy as sp
 import numpy as np
+import pentapy as pp
+import matplotlib.pyplot as plt
 
 class FEM_P2_solver():
     """
@@ -19,7 +21,7 @@ class FEM_P2_solver():
         # step size
         self.step =  interval/(self.n_nodes-1)
 
-        # Get midpoint and nodes mesh
+        # Get midpoint and nodes mesh (nodes are physical mesh points)
         self.nodes = [i*self.step for i in range(self.n_nodes)] # setup physical mesh point
         self.two_n_m = [self.nodes[i*self.oth+2]  + \
                         self.nodes[i*self.oth] for i in range(self.Ne)]
@@ -31,11 +33,12 @@ class FEM_P2_solver():
 
         # Set up element
         self.elements = [[i*self.oth,i*self.oth+1,i*self.oth+2] for i in range(self.Ne)]
-
-        # Set up final matrix A and right hand side b.
+        self.local_nodes = len(self.elements[0])
+        # Set up final matrix A, numerical solution u and right hand side b
         self.A = np.zeros((self.n_nodes,self.n_nodes))
-        self.b = np.zeros(self.n_nodes)
+        self.b = np.zeros(self.n_nodes);
         #print(self.A)#print(self.nodes_m)#print(self.h) #print(elements)
+        #print(self.nodes_m)
 
     def setup_element_matrix(self):
         """
@@ -44,6 +47,7 @@ class FEM_P2_solver():
         oth,h = self.oth,self.h;
         self.A_e = np.zeros((oth + 1,oth + 1))
         self.A_e[0,0] = self.A_e[self.oth,oth] = 7;
+        self.A_e[1,1] = 16;
         self.A_e[oth,1] = self.A_e[1,oth] = -8
         self.A_e[1,0] = self.A_e[0,1] = -8
         self.A_e[oth,0] = self.A_e[0,oth] = 1
@@ -57,6 +61,7 @@ class FEM_P2_solver():
         oth,h = self.oth, self.h
         self.A_n = np.zeros((oth + 1,oth + 1))
         self.A_n[0,0] = 7;
+        self.A_n[1,1] = 16;
         self.A_n[1,oth] = -8
         self.A_n[1,0] = self.A_n[0,1] = -8
         self.A_n[0,oth] = 1
@@ -68,17 +73,35 @@ class FEM_P2_solver():
         self.A_n = 1/(3*h)*self.A_n
 
 
-    def setup_basis(self):
+
+    def evaluate_num(self,x):
         """
-        sets up the reference basis
+        Evaluates the numerical solution at mesh points
         """
+        ## Decide what local node a globale node lies within with use of modulo
+        ## but here is the easier way, but maybe less efficient way chosen
+        ## Set up basis functions
+        self.psi = lambda x,a,b,c: (x-b)/(a-b)*(x-c)/(a-c);
+        ## numerical solution
+        self.u = np.zeros(len(x))
+        j = 0; i = 0
+        while j <= len(self.nodes)-1:
+            while i < len(x) and x[i] <= self.nodes[j+2]:
+                #print('j:',j)
+                #print('xi:',x[i])
+                #print('node:',self.nodes[j+2])
+                self.u[i] = self.c[j]*self.psi(x[i],self.nodes[j],self.nodes[j+1],self.nodes[j+2]) + \
+                self.c[j+1]*self.psi(x[i],self.nodes[j+1],self.nodes[j],self.nodes[j+2]) + \
+                self.c[j+2]*self.psi(x[i],self.nodes[j+2],self.nodes[j],self.nodes[j+1])
+                i += 1
+            j += 2
 
     def assemble(self):
         """
         Map to global system/assemble
         element matrices and element vectors
         """
-        local_nodes = len(self.elements[0])
+        local_nodes = self.local_nodes;
 
         # Integrate on referance
         self.find_b_ref();
@@ -86,61 +109,98 @@ class FEM_P2_solver():
         b_e = [b0_e,b1_e,b2_e]
 
         # setup element matrix
-        self.setup_element_matrixN;
+        self.setup_element_matrix();
 
-         # Assemble elements
-        for e in range(len(self.elements)-1):
+         # Assemble inner elements
+        for e in range(1,len(self.elements)-1):
             for r in range(local_nodes):
                 for s in range(local_nodes):
-                     self.A[self.elements[e][r], elements[e][s]] += self.A_e[r,s]
-                self.b[elements[e][r]] += b_e[r](self.nodes_m*e)
+                     self.A[self.elements[e][r], self.elements[e][s]] += self.A_e[r,s]
+                self.b[self.elements[e][r]] += b_e[r](self.nodes_m[e],self.h)
 
         ### Handle boundary conditions in first and last element ###
         # change rhs index 0 for Neumann boundary
-        e = 0; r = 0;
-        self.b[elements[e][r]] += b_e[r](self.nodes_m*e) - self.C
+        e = 0;
+        for r in range(local_nodes):
+            for s in range(local_nodes):
+                self.A[self.elements[e][r], self.elements[e][s]] += self.A_e[r,s]
+            if r != 0:
+                self.b[self.elements[e][r]] += b_e[r](self.nodes_m[e],self.h)
+            else:
+                self.b[self.elements[e][r]] += b_e[r](self.nodes_m[e],self.h) \
+                    - self.C
+
 
         # last element
         self.setup_element_matrixN();
-        e = self.elements-1;
+        e = self.Ne-1;
         for r in range(local_nodes):
             for s in range(local_nodes):
-                 self.A[self.elements[e][r], elements[e][s]] += self.A_n[r,s]
-            self.b[elements[e][r]] += b_e[r](self.nodes_m*e)
+                self.A[self.elements[e][r], self.elements[e][s]] += self.A_n[r,s]
+            self.b[self.elements[e][r]] += b_e[r](self.nodes_m[e],self.h)
 
         # Alter last index right hand side
-        r = len(local_nodes)-1
-        self.b[elements[e][r]] = self.D
+        r = local_nodes-1
+        self.b[self.elements[e][r]] = self.D
+        #print(self.b[self.elements[e][r]])
 
 
     def find_b_ref(self):
          x, x_m, h, X = sp.symbols('x x_m h X')         # make symbols
          x = x_m + h/2*X                                # Apply transformation
          wrt = (X, -1, 1)                               # Interval and variable
-         b_0 = sp.integrate(h*sp.Rational(1,4)*(x-sp.Rational(1,2))*(X-1)*X, wrt)   # b0
-         b_1 = sp.integrate(h*sp.Rational(1,4)*(x-sp.Rational(1,2))*(1-X**2), wrt)  # b1
-         b_2 = sp.integrate(h*sp.Rational(1,4)*(x-sp.Rational(1,2))*(X+1)*X, wrt)   # b2
+         b_0 = sp.integrate(h*sp.Rational(1,4)*(2*x-1)*(X-1)*X, wrt)   # b0
+         b_1 = sp.integrate(h*sp.Rational(1,2)*(2*x-1)*(1-X**2), wrt)  # b1
+         b_2 = sp.integrate(h*sp.Rational(1,4)*(2*x-1)*(X+1)*X, wrt)   # b2
 
          #print("Integrating with numerical python element vector in reference")
          #print("b0",b_0)
          #print("b1",b_1)
          #print("b2",b_2)
 
-         self.b0_e = sp.lambdify((x_m),b_0,'numpy')
-         self.b1_e = sp.lambdify((x_m),b_1,'numpy')
-         self.b2_e = sp.lambdify((x_m),b_2,'numpy')
+         self.b0_e = sp.lambdify((x_m,h),b_0,'numpy')
+         self.b1_e = sp.lambdify((x_m,h),b_1,'numpy')
+         self.b2_e = sp.lambdify((x_m,h),b_2,'numpy')
 
-    def solve(self):
+    def solve_linear(self, option = 0):
         """
-        solves the assembled system
+        solves the assembled system using either pentapy or numpy linalg solve
         """
-        # using LU or linalg solve?
-    def convergence_test(self):
+        if option == 0:
+            self.c = pp.solve(self.A,self.b, is_flat = False)
+        else:
+            self.c = np.linalg.solve(self.A,self.b)
+
+    def get_numerical_solution(self,x):
+        self.x = x;
+        self.assemble()
+        self.solve_linear()
+        self.evaluate_num(x)
+        return self.u
+
+    def exact(self,x):
+        constant = (self.D-self.C-1/6)
+        p3 = -1/3*x**3 + 1/2*x**2 + self.C*x
+        return p3 + constant
+
+    def L2_norm(self):
         """
-        write a convergence test here or make own class with inheritance
-        """
+        Calculate the L2-norm by approximating the integral as a sum
+        """                          # Interval and variable
+        L2 = np.sqrt(self.h*sum((self.exact(self.x)-self.u)**2))
+        return L2, self.h
 
     def plot(self):
         """
         Plot the numerical and analytical solution
         """
+        plt.plot(self.x,self.exact(self.x),'-',label = "analytical")
+        plt.plot(self.x,self.u,'--', label = "numerical")
+        plt.title("Finite Element Method")
+        plt.xlabel("x",fontsize = 13)
+        plt.ylabel("u",fontsize = 13)
+        plt.legend(loc = "upper left",fontsize = 15)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.tight_layout()
+        plt.show()
--- FEM1D_project/main.py
+++ FEM1D_project/main.py
@@ -1,11 +1,59 @@
 ### Use the FEM_P2_solver class here ###
 from FEM_P2 import FEM_P2_solver
+import numpy as np
+import sympy as sp
 
-Ne = 4
-C = 1
-D = 1
+print("\n")
+print("Solving Partial Differential Equation")
+x,C,D, u, d,dx = sp.symbols('x C D u d dx')
+print(-d**2*u/dx**2)
+print("=", 2*x-1)
+print("with solution")
+p3 = -sp.Rational(1,3)*x**3 + sp.Rational(1,2)*x**2 + C*x + D-C-sp.Rational(1,6)
+print(p3)
+print("applying the Finite Element Method. \n")
 
-derive_b = FEM_P2_solver(C,D,Ne)
-#derive_b.find_b_ref()
-#derive_b.setup_element_matrix()
-#derive_b.assemble()
+
+C = float(input("Input Neumann Boundary Condition, C:"));
+D = float(input("Input Dirichlet Boundary Condition, D:"));
+Ne = int(input("Choose number of elements (int):"));
+
+x = np.linspace(0,1, 10000)
+pdenum = FEM_P2_solver(C,D,Ne)
+pdenum.get_numerical_solution(x)
+
+print("Press 1 to plot");
+print("Press 2 to run a convergence test")
+arg = input("Input number:")
+
+if int(arg) == 1:
+    pdenum.plot()
+elif int(arg) == 2:
+    N = int(input("Input number of experiments (int):"))
+
+    # setup arrays to store h and l2
+    l2_mesh = np.zeros(N); h_mesh = np.zeros(N)
+
+    for i in range(N):
+        norm = FEM_P2_solver(C,D,Ne)
+        norm.get_numerical_solution(x)
+        l2,h = norm.L2_norm()
+
+        # Update Ne
+        Ne = 4*Ne
+
+        # Store l2 and h
+        l2_mesh[i] = l2
+        h_mesh[i] = h
+
+    # Calculate convergence rates
+    r = np.zeros(N)
+    r = [np.log(l2_mesh[i]/l2_mesh[i-1])/np.log(h_mesh[i]/h_mesh[i-1]) \
+    for i in range(1,N)]
+    print(r)
+    # print out table
+    print('h            l2-norm     convergence rate')
+    print("{:.4e}   {:.4e}  ---------".format(h_mesh[0],l2_mesh[0],))
+
+    for i in range(1,N):
+        print("{:.4e}   {:.4e}  {:.4e}".format(h_mesh[i],l2_mesh[i],r[i-1]))
